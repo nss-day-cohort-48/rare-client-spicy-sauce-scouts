19187c9fc2f83e04b7fe640307dee3831b6a6563
Justin <justinadams758@gmail.com>
2021-07-26 14:11:34 -0500

    register page setup

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -50,7 +50,7 @@ export const Register = (props) => {
             </dialog>
 
             <form className="form--login" onSubmit={handleRegister}>
-                <h1 className="h3 mb-3 font-weight-normal">Register an account</h1>
+                <h1 className="h3 mb-3 font-weight-normal">Register An Account</h1>
                 <fieldset>
                     <label htmlFor="firstName"> First Name </label>
                     <input ref={firstName} type="text" name="firstName" className="form-control" placeholder="First name" required autoFocus />
