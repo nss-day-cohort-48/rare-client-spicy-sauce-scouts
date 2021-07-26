c09ecf3d20204f71b29a18b4c7d6eddf8715ee1e
Justin <justinadams758@gmail.com>
2021-07-26 14:13:57 -0500

    register page changes

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -21,7 +21,7 @@ export const Register = (props) => {
                 "email": email.current.value,
             }
 
-            return fetch("http://127.0.0.1:8088/register", {
+            return fetch("http://localhost:8088/users", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
