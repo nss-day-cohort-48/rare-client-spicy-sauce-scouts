d0c6cffab1da24a013e02c2d583108f0a7747bdd
Tom Clouse <tommyclouse@yahoo.com>
2021-08-17 15:31:17 -0500

    still working

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -15,8 +15,7 @@ export const Login = () => {
         return fetch("http://localhost:8000/login", {
             method: "POST",
             headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
+                "Content-Type": "application/json"
             },
             body: JSON.stringify({
                 username: username.current.value,
