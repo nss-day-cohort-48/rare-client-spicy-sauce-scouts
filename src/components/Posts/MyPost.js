00d0c40f6ea221a12b0bc3da0363b22061118249
Justin <justinadams758@gmail.com>
2021-08-19 13:20:10 -0500

    delete works now

--- src/components/Posts/MyPost.js
+++ src/components/Posts/MyPost.js
@@ -17,28 +17,6 @@ export const MyPostList = props => {
         let filter = posts.filter(post => post.user.username === currentUsername)
         setFilteredPosts(filter)
       }, [posts])
-      
-      const editPostButton = (user_id, post_id) => {
-        if (user_id == localStorage.getItem("rare_user_id")) {
-          return <button
-          className="post blueText"
-          id={`post--${post_id}`}
-          onClick={(event) => {
-            event.preventDefault();
-            handleUpdatePost(post_id);
-          }}
-          >
-            Edit Post
-          </button>
-          }
-          else {
-            return 
-          }
-        }
-        
-        const handleUpdatePost = (post_id) => {
-          history.push(`/posts/edit/${post_id}`);
-        };
         
         
         if (posts.length > 0) {
@@ -64,7 +42,6 @@ export const MyPostList = props => {
                         <Link to={`/posts/${post.id}`}>{post.title}</Link>
                         <div>{post.category.label}</div>
                         <div>{post.user.first_name} {post.user.last_name}</div>
-                        {editPostButton(post.user_id, post.id)}
                         </article>
                         </>
                     )
